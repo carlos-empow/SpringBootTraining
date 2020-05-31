2a146b7792ce11423cfea43eddd02c5f64c3f0bf
Carlos Guerra <c.guerra@accenture.com>
2020-05-30 20:07:51 -0500

    Exercise 6

--- mongoconfig-client/src/main/java/com/springtraining/client/MongoConfigClientApplication.java
+++ mongoconfig-client/src/main/java/com/springtraining/client/MongoConfigClientApplication.java
@@ -2,7 +2,9 @@ package com.springtraining.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MongoConfigClientApplication {
 
